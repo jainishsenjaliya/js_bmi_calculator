7f7fa658074bef4b0a6e8605d6772ea8bd342d6e
jainish <jainishsenjaliya@gmail.com>
2016-04-23 17:18:59 +0530

    Modify client side validation

--- Resources/Public/Script/Bmi.js
+++ Resources/Public/Script/Bmi.js
@@ -15,6 +15,13 @@
 				jQuery(jQuery(this)).addClass("error");
 				jQuery(this).attr("placeholder",jQuery(this).attr('mendatory_message'));
 				cValids = 1;
+			}else{ 
+				if(!validate(jQuery(this).val())){
+					jQuery(jQuery(this)).addClass("error");
+					jQuery(this).attr("placeholder",jQuery(this).attr('valid_message'));
+					jQuery(this).val("");
+					cValids = 1;
+				}
 			}
 		});
 
@@ -30,6 +37,40 @@
 		return jQuery.trim(val);	
 	}
 
+	function validate(s) {
+		var rgx = /^[0-9]*\.?[0-9]*$/;
+		return s.match(rgx);
+	}
+
 jQuery(document).ready(function() {
 
+	jQuery(".validate").blur(function(){
+		
+		if(trim(jQuery(this).val())!=""){
+			
+			jQuery(this).removeClass("error");
+
+			if(!validate(jQuery(this).val())){
+				jQuery(jQuery(this)).addClass("error");
+				jQuery(this).attr("placeholder",jQuery(this).attr('valid_message'));
+				jQuery(this).val("");
+				cValids = 1;
+			}
+		}else{
+			jQuery(this).addClass("error");
+			jQuery(this).attr("placeholder",jQuery(this).attr('mendatory_message'));
+			jQuery(this).val("");
+		}
+	})
+
+	jQuery(".validate").keyup(function(){
+		
+		if(trim(jQuery(this))!=""){
+			jQuery(this).removeClass("error");
+		}else{
+			jQuery(this).addClass("error");
+		}
+	})
+
+	jQuery(".successMessage").delay(9000).hide(500);
 });
