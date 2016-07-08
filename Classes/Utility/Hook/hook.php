da8fc0a73b6befb73ba34d67522879c2491a9fa9
jainishsenjaliya <jainish.online@gmail.com>
2016-07-08 20:22:41 +0530

    Added elements to the Content Element Wizard.

--- Classes/Utility/Hook/hook.php
+++ Classes/Utility/Hook/hook.php
@@ -9,3 +9,11 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignat
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/Flexform/FlexForm.xml');
 
 /* END: include flexform */
+
+
+// Add wizard icon to the "Add new record" in backend
+
+if (TYPO3_MODE == "BE") {
+	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["JS\JsBmiCalculator\Utility\Hook\ContentElementWizard"] =
+		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Utility/Hook/ContentElementWizard.php';
+}
