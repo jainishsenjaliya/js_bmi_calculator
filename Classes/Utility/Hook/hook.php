3987d9c547daee6cf33aed1a89e1fbe158057e06
jainishsenjaliya <jainish.online@gmail.com>
2016-08-13 17:57:32 +0530

    Added plugin information.

--- Classes/Utility/Hook/hook.php
+++ Classes/Utility/Hook/hook.php
@@ -17,3 +17,8 @@ if (TYPO3_MODE == "BE") {
 	$TBE_MODULES_EXT["xMOD_db_new_content_el"]["addElClasses"]["JS\JsBmiCalculator\Utility\Hook\ContentElementWizard"] =
 		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath($_EXTKEY) . 'Classes/Utility/Hook/ContentElementWizard.php';
 }
+
+// Hook to show Plugin Information
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['cms/layout/class.tx_cms_layout.php']['list_type_Info']['jsbmicalculator_bmi'][$_EXTKEY] =
+	'EXT:' . $_EXTKEY . '/Classes/Utility/Hook/PluginInformation.php:JS\JsBmiCalculator\Utility\Hook\PluginInformation->build';
